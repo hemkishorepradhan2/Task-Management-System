be154992b9dbbd1ed6935bfd3c95a041ae070a84
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-17 12:31:27 +0530

    Done with including all the api routes.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from database.connection import engine, Base
+from router.task_router import router as task_router
 
-app=FastAPI()
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title="Task Management API")
+app.include_router(task_router)
 
-@app.get("/")
-def home_route():
-    return {"message":"WELCOME TO TASK MANAGEMENT SYSTEM"}
