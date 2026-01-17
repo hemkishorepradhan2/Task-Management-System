a9ec7bfab41a8e40451359fa23d10d7d798c21ef
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-17 14:17:45 +0530

    fixed double trailing / issues.

--- backend/router/task_router.py
+++ backend/router/task_router.py
@@ -1,52 +1,40 @@
-from fastapi import APIRouter,HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from database.session import get_db
 from schemas.task import CreateTask, UpdateTask, TaskResponse
 from crud.crud import create_task, read_tasks, read_task, update_task, soft_delete_task
-from typing import List, Optional
-
-
+from typing import List
 
 router = APIRouter(
     prefix="/tasks",
     tags=["Tasks"]
 )
 
-
-@router.post("/", response_model=TaskResponse)
+@router.post("", response_model=TaskResponse)
 def create_new_task(task_in: CreateTask, db: Session = Depends(get_db)):
     return create_task(db, task_in)
 
-
-@router.get("/", response_model=List[TaskResponse])
+@router.get("", response_model=List[TaskResponse])
 def get_all_tasks(db: Session = Depends(get_db)):
-    tasks_list= read_tasks(db)
-    if not tasks_list:
-        raise HTTPException(status_code=404, detail="No tasks found")
-    return tasks_list
+    return read_tasks(db)
 
-
-@router.get("/{task_id}", response_model=Optional[TaskResponse])
+@router.get("/{task_id}", response_model=TaskResponse)
 def get_task_by_id(task_id: int, db: Session = Depends(get_db)):
-    task= read_task(db, task_id)
+    task = read_task(db, task_id)
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
-
-@router.put("/{task_id}", response_model=Optional[TaskResponse])
+@router.put("/{task_id}", response_model=TaskResponse)
 def update_task_by_id(task_id: int, task_in: UpdateTask, db: Session = Depends(get_db)):
-    updated_task= update_task(db, task_id, task_in)
+    updated_task = update_task(db, task_id, task_in)
     if not updated_task:
         raise HTTPException(status_code=404, detail="Task not found")
     return updated_task
 
-@router.delete("/{task_id}", response_model=Optional[TaskResponse])
+@router.delete("/{task_id}", response_model=TaskResponse)
 def delete_task_by_id(task_id: int, db: Session = Depends(get_db)):
-    deleted_task= soft_delete_task(db, task_id)
+    deleted_task = soft_delete_task(db, task_id)
     if not deleted_task:
         raise HTTPException(status_code=404, detail="Task not found")
     return deleted_task
-
-
-
