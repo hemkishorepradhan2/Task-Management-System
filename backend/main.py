14d9adb9f6f46af7f17c0347150f99348e4e8001
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-17 13:34:40 +0530

    Fixed cors error + integerated frontend with backend.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from database.connection import engine, Base
 from router.task_router import router as task_router
 
 Base.metadata.create_all(bind=engine)
-app = FastAPI(title="Task Management API")
-app.include_router(task_router)
 
+app = FastAPI(title="Task Management API")
+
+
+origins = [
+    "http://localhost:3000",   
+    "http://localhost:5173",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(task_router)
--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import { createTask } from "../api/taskApi";
 
-function TaskForm() {
+const TaskForm = ({ refreshTasks }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await createTask({
+      title,
+      description,
+      due_date: dueDate,
+      status: "Pending",
+    });
+
+    setTitle("");
+    setDescription("");
+    setDueDate("");
+    refreshTasks();
+  };
+
   return (
-    <div>TaskForm</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <h3>Create Task</h3>
 
-export default TaskForm
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <input
+        type="date"
+        value={dueDate}
+        onChange={(e) => setDueDate(e.target.value)}
+      />
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default TaskForm;
