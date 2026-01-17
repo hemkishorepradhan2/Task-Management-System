65d35310a254351146ba2b076ac9ea6664825ff5
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-17 13:17:31 +0530

    done with the form prerequisite.

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 function TaskForm() {
-  return (
-    <div>TaskForm</div>
+    const [name,setName]=useState("");
+    
+
+    return (
+   
+    <>
+    <form>
+        <label>
+            Enter your name: 
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+        </label>
+    </form>
+    <p>Hello {name} </p>
+    </>
   )
 }
 
-export default TaskForm
+export default TaskForm;
