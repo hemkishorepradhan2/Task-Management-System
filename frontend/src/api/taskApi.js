c40de48b5113e5493e661fce6db396bb83503730
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-17 14:20:17 +0530

    fixed axios not return bug.

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,21 +1,14 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL="http://localhost:8080/tasks";
+const API_URL = "http://localhost:8080/tasks";
 
-export const getTasks=()=>{
-axios.get(API_URL);
-}
+export const getTasks = () => axios.get(API_URL);
 
-export const createTask=(task)=>{
-    axios.post(API_URL,task);
-}
+export const createTask = (task) =>
+  axios.post(API_URL, task);
 
+export const updateTask = (id, updatedData) =>
+  axios.put(`${API_URL}/${id}`, updatedData);
 
-export const updateTask=(id,updatedData)=>{
-    axios.put(`${API_URL}/${id}`,updatedData);
-}
-
-
-export const deleteData=(id)=>{
-    axios.delete(`${API_URL}/${id}`);
-}
+export const deleteTask = (id) =>
+  axios.delete(`${API_URL}/${id}`);
