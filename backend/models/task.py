3cfe9dde56ba70dacbc276b7c6567650e0b87c61
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-17 11:35:18 +0530

    Done with defining table structure for tasks + dbdependency.

---
+++ backend/models/task.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime,Boolean
+from database.connection import Base
+
+Base=declarative_base()
+class Task(Base):
+    __tablename__="tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    status = Column(String, nullable=False, default="PENDING")
+    due_date = Column(DateTime, nullable=True)
+    is_deleted = Column(Boolean, nullable=False, default=False)
